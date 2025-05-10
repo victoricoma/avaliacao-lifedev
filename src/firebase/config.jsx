00bf2c78f4eb8a82b89b455a776f678da3196c1c
RamonSantos10 <ramontedescodossantos2020@gmail.com>
2025-05-10 06:12:29 -0300

    chore: update Firebase configuration and add new files
    - Added firestore.indexes.json and firestore.rules for Firestore setup
    - Updated Firebase SDK version in package.json to ^11.7.1
    - Configured Firebase credentials in config.jsx
    - Added dist/index.html for Firebase Hosting setup

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-};
+    apiKey: "AIzaSyA0nLdjrdHx_votKLcZX81q8IWC8XlzEXA",
+    authDomain: "study-workspace-66833.firebaseapp.com",
+    projectId: "study-workspace-66833",
+    storageBucket: "study-workspace-66833.firebasestorage.app",
+    messagingSenderId: "630811162274",
+    appId: "1:630811162274:web:1fcb8dff24088f93ad89fb",
+    measurementId: "G-DN1R1Q1S3G"
+  };
 
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
